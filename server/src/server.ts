62167e360413c3e31c731b49a2b7b54503aafcad
Victor Humberto <107077339+Victorhumbert@users.noreply.github.com>
2025-07-11 13:37:19 -0300

    configurado drizzle-seed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,24 @@
-import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { fastifyCors } from "@fastify/cors";
+} from 'fastify-type-provider-zod';
+import { sql } from './db/connection.ts';
+import { env } from './env.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: "http://localhost:5173",
+  origin: 'http://localhost:5173',
 });
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-app.listen({port: 3000}).then(() => {
-    console.log("HTTP server is running!")
-})
+app.get('/health', () => {
+  return 'OK';
+});
+
+app.listen({ port: env.PORT });
